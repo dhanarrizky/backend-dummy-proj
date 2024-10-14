6afd76837ce23d73ba576c0eb1266ae086b23a66
dhanarrizky <dhanarrizky23@gmail.com>
2024-10-15 06:17:57 +0700

    adding some controller

---
+++ todo-list/todolist__dotnet/Controllers/Auth/LoginController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace todolist__dotnet.Controllers.Auth;
+
+[Route("api/auth")]
+[ApiController]
+public class LoginController : ControllerBase {
+    [HttpPost("login")]
+    public IActionResult Login(){
+        return Ok(new { message = "test" });
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(){
+        return Ok();
+    }
+}
