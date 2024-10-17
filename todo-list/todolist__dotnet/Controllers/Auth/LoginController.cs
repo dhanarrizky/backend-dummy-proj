d6b06088fc15ec7739c0a7c4b167e03e7e6bfddd
dhanarrizky <dhanarrizky23@gmail.com>
2024-10-18 06:33:39 +0700

    updated some package

--- todo-list/todolist__dotnet/Controllers/Auth/LoginController.cs
+++ todo-list/todolist__dotnet/Controllers/Auth/LoginController.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using todolist__dotnet.Models.Request;
+using todolist__dotnet.Services.Auth;
 
 namespace todolist__dotnet.Controllers.Auth;
 
 [Route("api/auth")]
 [ApiController]
 public class LoginController : ControllerBase {
+
+    private readonly  LoginServices _services;
+    public LoginController(LoginServices services) {
+        _services = services;
+    }
+
     [HttpPost("login")]
-    public IActionResult Login(){
-        return Ok(new { message = "test" });
+    public IActionResult Login(LoginRequestModel md){
+        var res = _services.LoginService(md);
+        return Ok(res);
     }
 
     [HttpPost("register")]
---
+++ todo-list/todolist__dotnet/Data/DataContext.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace todolist__dotnet.Data;
+
+public class DataContext : IdentityDbContext {
+    
+}
