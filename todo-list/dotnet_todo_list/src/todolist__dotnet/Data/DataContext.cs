d21be498232b71e25c0c69ce60ee0bad3fad56d3
dhanarrizky <dhanarrizky23@gmail.com>
2024-10-25 08:26:04 +0700

    updated for db

--- todo-list/dotnet_todo_list/src/todolist__dotnet/Data/DataContext.cs
+++ todo-list/dotnet_todo_list/src/todolist__dotnet/Data/DataContext.cs
@@ -3,5 +3,20 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 namespace todolist__dotnet.Data;
 
 public class DataContext : IdentityDbContext {
-    
+    // static async Task Main(string[] args)
+    //     {
+    //         var connString = "Host=localhost;Username=your_username;Password=your_password;Database=your_database";
+
+    //         using var conn = new NpgsqlConnection(connString);
+    //         await conn.OpenAsync();
+
+    //         // Command untuk memanggil stored procedure
+    //         using var cmd = new NpgsqlCommand("CALL insert_account(@p_name, @p_email)", conn);
+    //         cmd.Parameters.AddWithValue("p_name", "John Doe");
+    //         cmd.Parameters.AddWithValue("p_email", "johndoe@example.com");
+
+    //         await cmd.ExecuteNonQueryAsync();
+
+    //         Console.WriteLine("Stored procedure executed successfully!");
+    //     }
 }
